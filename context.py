1aecf8d62d6202983a4ee137174e1ae60695b9f9
yifan96 <yifanbai96@gmail.com>
2022-11-16 14:57:32 +0100

    copy paste zhongqiang Ren's MSPF and MCPF algorithm

---
+++ context.py
@@ -0,0 +1,9 @@
+"""
+Author: Zhongqiang (Richard) Ren
+All Rights Reserved.
+Oeffentlich fuer: RSS22
+"""
+
+import os, sys
+sys.path.append(os.path.abspath('./libmcpf'))
+sys.path.append(os.path.abspath('./pytspbridge'))
